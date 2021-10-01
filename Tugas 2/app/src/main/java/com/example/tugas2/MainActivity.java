92f6c12f85bb55179cb6d5e5062cba3d5e357143
arifrizaldi <akram.pentol7@gmail.com>
2021-10-01 16:56:20 +0700

    Pengumpulan Tugas 2

---
+++ Tugas 2/app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView angkaKounter;
+    int angka = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        angkaKounter = findViewById(R.id.kounter);
+    }
+
+    public void toastklik(View view) {
+        Toast.makeText(view.getContext(), "ini adalah toast", Toast.LENGTH_LONG).show();
+    }
+
+    public void btnkounter(View view) {
+        angka = angka + 1;
+        angkaKounter.setText(Integer.toString(angka));
+    }
+}
